41d717387310d9cacc9ac491d83c1025b58a8c3f
Ayush <ayushempire03@gmail.com>
2025-01-04 10:22:54 +0530

    update initial route from Otp to Login in navigation stack

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Otp">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Login"
           component={Login}
